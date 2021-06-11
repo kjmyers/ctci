89d2202d813c78fbf5a81d4a7b1af722097ccd01
kjmyers <kevin.j.myers@gmail.com>
2021-06-10 22:36:44 -0400

    Add solution to ch1 problem 5

---
+++ ch1_5/ch1_5.js
@@ -0,0 +1,49 @@
+/* Problem 5 ch1
+One Away:
+There are three types of edits that can be performed on strings:
+Insert a character, remove a character, or replace a character.
+Given two strings, write a function to check if they are one edit (or zero edits) away
+*/
+
+function oneAway(string1, string2) {
+
+    console.log(string1)
+    console.log(string2)
+
+    var errorCount = 0
+    
+    if(string1.length - string2.length > 1) {
+        return false
+    }
+    else if(string1.length - string2.length == 1) {
+        errorCount++
+    }
+
+    if(string2 > string1) {
+        longer = string2
+        shorter = string1
+    }
+    else {
+        longer = string1
+        shorter = string2
+    }
+
+    for(letter in longer) {
+        console.log(longer[letter])
+        if(longer[letter] != shorter[letter]) {
+            errorCount++
+            if(errorCount > 1) {
+                return false
+            }
+        }
+    }
+    console.log(errorCount)
+    return true
+
+}
+
+
+string1 = "This is a mone"
+string2 = "This is a line"
+
+console.log(oneAway(string1, string2))
